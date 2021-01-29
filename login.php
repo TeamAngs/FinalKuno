d5925f27da8fdb4705b7a391a75c88bf1b1a83e4
thatswhathesaid <realchimin23@gmail.com>
2021-01-29 15:57:05 +0800

    UPDATE 01/29

--- login.php
+++ login.php
@@ -7,24 +7,37 @@
 <body>
 <div id="loginbox"> 
     <form action = "login_code.php" method = "post">
-    <img src="images/icon.png" class="icon">
-        <p>
-            <label>Username</label>
-                <input type = "text" id = "username" name = "username" required=""/>
-        </p>
-        <p>
-            <label>password</label>
+        <img src="images/icon.png" class="icon">
+            <!-- <p>
+                <label>Username</label>
+                    <input type = "text" id = "username" name = "username" required=""/>
+            </p>
+            <p>
+                <label>password</label>
                 <input type = "text" id = "pwd" name = "password" required=""/>
-        </p>
+            </p>
           
-        <button type="submit" name="login" class="b btn-primary">Login</button>
+            <button type="submit" name="login" class="b btn-primary">Login</button> -->
+        <h1>Welcome!</h1>
+        <form >
+        <p>Username</p>
+            <input type="text" name="" placeholder="Enter Username">
+        <p>Password</p>
+            <input type="password" name="" placeholder="Enter Password">
+            <input type="submit" name="Login" value="Login">
+            <a href="registration.php">Don't have an account?</a><br>
+            <a href="#">Forget Password</a>
+        </form>
     </form>
   
-    <form action = "registration.php" method = "post">
-        <p>
-            <button type="submit" name="register" class="btn btn-primary">Register</button>
-        </p>
-    </form>   
+    <!-- <div>
+        <form action = "registration.php" method = "post">
+            
+        <button type="submit" name="register" value="Register">Register</button>
+        </form>
+    </div>    -->
+    <!-- <a href="registration.php">Don't have an account?</a> -->
+
 </div>
 </body>
 </html>
