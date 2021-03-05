9f8b4ebb4735677c41cad42b5b75c3348f5e8ce4
kamotejhon <jhonbustamante011501@gmail.com>
2021-03-05 14:08:46 +0800

    wala lang

--- login.php
+++ login.php
@@ -1,9 +1,10 @@
 <html>
 <head>
+</style>
     <title> Dental.io </title>
     <meta charset="UTF-8">
     <link rel = "stylesheet" href = "style1.css">
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"> -->
+</style>
 </head>
 <body>
 <marquee behavior="scroll" direction="left"><img src="//www.html.am/images/html-codes/marquees/fish-swimming.gif" width="94" height="88" alt="Swimming fish" /></marquee>
@@ -12,19 +13,19 @@
             <img src="images/icon.png" class="icon">
             <h1>Welcome!</h1>
             <div class="form-group <?= (!empty($errorMessage) || !empty($usernameErr)) ? 'has-error' : ''; ?>" >
-                <label>Email Address</label>
+                <label><p>Email Address</p></label>
                 <input type="text" name="Email" placeholder="Example: Example@gmail.com"  required="">
                 <span class = "help-block"></span>
             </div>
 
             <div class="form-group">
-                <label>Password</label> 
+                <label><p>Password</p></label> 
                 <input type="password" name="password" placeholder="Enter Password" required="">
                 <span class = "help-block"></span>
             </div>
 
             <div class="form-group">
-                <input type="submit" class="btn btn-primary" name="Login" value="Login">
+                <input type="submit" name="Login" value="Login">
             </div>
             <a href="registration.php">Don't have an account?</a><br>
             <a href="#">Forget Password</a>
@@ -32,4 +33,3 @@
     </div>
 </body>
 </html>
-<!-- ss -->
